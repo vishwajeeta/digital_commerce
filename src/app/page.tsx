bdec056b8fd5b1a75d58d48793820e3ff2d67c4f
vishwajeeta <gs699542@gmail.com>
2024-08-02 08:06:04 +0530

    added some styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,22 @@
 "use client"
+import MaxWidthWrapper from '@/components/MaxWidthWrapper'
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   
   
   
   
-  return (
-    <>
-    </>
-  )
+  return <MaxWidthWrapper>
+    <div className='py-20 mx-auto text-center flex flex-col items-center max-w-3xl'>
+      <h1 className='text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl'>
+        Your Market place for <span className='text-blue-600'> digital </span> Products/Assets.</h1>
+        <p className='mt-6 text-lg max-w-prose text-muted-foreground'>Welcome to DigitalMarket. Every asset on our platform is unverified by our team to ensure Their freedom and standards</p>
+        <div className='flex flex-col sm:flex-row gap-4 mt-6'>
+          <Link href='/products'>Browse Trending</Link>
+        </div>
+    </div>
+
+  </MaxWidthWrapper>
 }
