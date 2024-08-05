9bc64ada239c79336f5b75f2c95e8aa69ae7e774
vishwajeeta <gs699542@gmail.com>
2024-08-05 12:56:15 +0530

    updated fonts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,71 @@
-"use client"
-import MaxWidthWrapper from '@/components/MaxWidthWrapper'
-import Image from 'next/image'
-import Link from 'next/link'
+"use client";
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import { Button, buttonVariants } from "@/components/ui/button";
+import { Ghost } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+
+const perks=[
+  {
+    name:'Instant Delivery',
+    description:'Get your assets delivered to your email in seconds and download them right away.',
+  },
+  {
+    name:'Guaranteed Quality',
+    description:'Every asset on our platform is verified by our team to ensure our hightest quality standard.',
+  },
+  {
+    name:'save environment',
+    description:'we promote digital products and save trees.',
+  },
+]
 
 export default function Home() {
-  
-  
-  
-  
-  return <MaxWidthWrapper>
-    <div className='py-20 mx-auto text-center flex flex-col items-center max-w-3xl'>
-      <h1 className='text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl'>
-        Your Market place for <span className='text-blue-600'> digital </span> Products/Assets.</h1>
-        <p className='mt-6 text-lg max-w-prose text-muted-foreground'>Welcome to DigitalMarket. Every asset on our platform is unverified by our team to ensure Their freedom and standards</p>
-        <div className='flex flex-col sm:flex-row gap-4 mt-6'>
-          <Link href='/products' className={''}>Browse Trending</Link>
+  return (
+    <>
+      <MaxWidthWrapper>
+        <div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl">
+          <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+            Your Market place for{" "}
+            <span className="text-blue-600"> digital </span> Products/Assets.
+          </h1>
+          <p className="mt-6 text-lg max-w-prose text-muted-foreground">
+            Welcome to DigitalMarket. Every asset on our platform is unverified
+            by our team to ensure Their freedom and standards
+          </p>
+          <div className="flex flex-col sm:flex-row gap-4 mt-6">
+            <Link href="/products" className={buttonVariants()}>
+              Browse Trending
+            </Link>
+            <Button variant="ghost">Our quality promise &arr;</Button>
+          </div>
         </div>
-    </div>
 
-  </MaxWidthWrapper>
+        {/*  */}
+      </MaxWidthWrapper>
+      <section className="border-t border-gray-200 bg-gray-50">
+        <MaxWidthWrapper className="py-20">
+          <div className="grid grid-cols-1 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-3 lg:gap-x-8 lg:gap-y-0">
+            {perks.map((perk)=>(
+              <div key={perk.name} className="text-center md:flex md:items-start md:text-left lg:block lg:text-center">
+                <div className="md:flex-shrink-0 flex justify-center">
+                  <div className="h-16 flex items-center justify-center rounded-full bg-blue-100 text-blue-900">
+
+                  </div>
+                </div>
+                <div className="mt-6 md:ml-4 md:mt-0 lg:ml-0 lg:mt-6">
+                  <h3 className="text-base font-medium text-grey-900">
+                    {perk.name}
+                  </h3>
+                  <p className="mt-3 text-sm text-muted-foreground">
+                    {perk.description}
+                  </p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </MaxWidthWrapper>
+      </section>
+    </>
+  );
 }
