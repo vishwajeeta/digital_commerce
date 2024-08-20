621dc180d1b0481419f683d58914c126c3d13900
vishwajeeta <gs699542@gmail.com>
2024-08-20 14:47:11 +0530

    updated

--- src/server.ts
+++
@@ -1,9 +0,0 @@
-import express from "express"
-const app=express()
-const PORT= Number(process.env.PORT) || 3000
-
-const start= async ()=>{
-    const payload= await getPayloadClient()
-}
-
-start()
